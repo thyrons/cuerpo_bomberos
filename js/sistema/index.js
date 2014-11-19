45818d2ddde2b476b958ee8568e2a4d9f1497f30
Willy Narvaez <willynarvaez6@gmail.com>
2014-11-19 09:14:50 -0500

    cambios
    evento key

--- js/sistema/index.js
+++ js/sistema/index.js
@@ -59,7 +59,7 @@ function inicio(){
 	/*--------------*/
 	/*funcion solu numeros*/
 	$(".soloNumeros").keydown(function(event){
-	    if((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode == 9) || (event.keyCode == 116) || (event.keyCode == 8)) {
+	    if((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode == 9) || (event.keyCode == 116) || (event.keyCode == 8) || (event.keyCode >= 96 && event.keyCode <= 105) || (event.keyCode == 110) || (event.keyCode == 190)) {
 	        return true;
 	    }else{
 	    	return false;
